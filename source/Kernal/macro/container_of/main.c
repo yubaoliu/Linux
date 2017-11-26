34a8960db32456b5eee5aa25680849af475c5d5a
yubaochina <yubaoliu89@gmail.com>
2017-11-27 06:24:22 +0800

    container_of

--- source/Kernal/macro/container_of/main.c
+++ source/Kernal/macro/container_of/main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #define offsetof(type, member) (size_t)&(((type*)0)->member)
 
-struct
+#ifndef container_of
+#define container_of(ptr,type,member)({\
+    const typeof(((type*) 0)->member)* __mptr=(ptr);    \
+    (type*)((char*)__mptr-offsetof(type,member));    \
+})
+#endif
+
+#ifndef container_of_simple
+    #define container_of_simple(ptr,type,member)  ((type*)((char*)ptr-offsetof(type,member)))
+#endif
+struct ST
 {
     int i;
     char c;
@@ -45,5 +55,25 @@ int main(int argc, char *argv[])
     comma_operator_common_C();
     GNU_extended_C();
     type_of();
+
+    struct ST s={0};
+    char* pc=&s.c;
+    struct ST* pst=container_of(pc, struct ST,c);
+    printf("&s=%p\n",&s);
+    printf("pst=%p\n",pst);
+
+    struct ST* pst_simple=container_of_simple(pc,struct ST,c);
+    printf("pst_simple=%p\n",pst_simple);
+
+    //Datatype check (unsafe)
+    int e=0;
+    int *pe=&e;
+    struct ST* pst_error=container_of_simple(pe,struct ST,c);
+    printf("pst_error=%p\n",pst_error);
+
+    //compile warning
+    struct ST* pst_error_report=container_of(pe,struct ST,c);
+    printf("pst_error_report=%p\n",pst_error_report);
+
     return 0;
 }
